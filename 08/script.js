33ef816cde89fe619bed84749daad8fc2be880ad
Nastya <doroshenco.anastasya@kmrf.kiev.ua>
2022-03-29 20:39:57 +0200

    29.03.2022

---
+++ 08/script.js
@@ -0,0 +1,37 @@
+/*Приклад 1*/
+document.write(123);
+/*Приклад 2*/
+document.write('text');
+/*Приклад 3*/
+document.write('<b>text</b>');
+/*Приклад 4*/
+let str1 = 'text';
+document.write(str1);
+/*Приклад 5*/
+let str2 = 'text';
+document.write('<b>' + str2 + '</b>');
+/*Приклад 6*/
+document.write('text <br> text');   
+/*Приклад 7*/
+document.write('text1<br>');
+document.write('text2<br>');
+document.write('text3<br>');
+/*Приклад 8*/
+document.write('text');
+document.write('<br>');
+document.write('text');
+
+document.write('<br><b>Завдання</b> <br>');
+
+/*Завдання 1*/
+document.write('Слава Україн!Героям слава! <br>');
+/*Завдання 2*/
+document.write('<i>Слава Україн!Героям слава!</i> <br>');
+/*Завдання 3*/
+let str3 = 'text';
+document.write(`<i>${str3}</i> <br>`);
+/*Завдання 4*/
+
+for(var i = 1; i < 6; i++){
+    document.write(`${i}<br>`);
+}
